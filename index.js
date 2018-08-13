f3b4b954b6edb319eba0d8e1efabd83747d1e5e3
William Tenório <william.tenorio@fcamara.com.br>
2018-08-13 10:48:36 -0300

    rota com chat

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 4000;
 
-app.get('/', function(req, res){
+app.get('/chat/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
